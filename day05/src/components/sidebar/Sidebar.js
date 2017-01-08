f5bf69828aca85cf42bf08df20c8e089a335ab34
Henrique Jensen <henriquejensen@hotmail.com>
2017-01-07 23:42:46 -0200

    ajuste open menu day 5

--- day05/src/components/sidebar/Sidebar.js
+++ day05/src/components/sidebar/Sidebar.js
@@ -3,38 +3,67 @@ import React, { Component } from "react";
 import './sidebar.css';
 
 export default class Sidebar extends Component {
-    state = {
-        isMenuOpen: false
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            menusOpened: []
+        }
+
+        this.onClickMenu = this.onClickMenu.bind(this);
+    }
+
+    onClickMenu(key) {
+        if(this.state.menusOpened.includes(key)) {
+            let newMenu = this.state.menusOpened.concat();
+            newMenu.pop(key);
+
+            return this.setState({
+                menusOpened: newMenu
+            })
+        }
+
+        this.setState({
+            menusOpened: this.state.menusOpened.concat(key)
+        })
     }
 
     render() {
+        const items = [
+            {label: "Home", link: "/", submenu: []},
+            {label: "About me", link: "", submenu: [
+                {label: "GitHub", link:"https://github.com/henriquejensen"},
+                {label: "Facebook", link: "https://facebook.com"}
+            ]},
+            {label: "Teste Menu", link: "", submenu: [
+                {label: "subitem 1", link: "#"},
+                {label: "subitem 2", link: "#"}
+            ]},
+        ]
+
         return (
             <ul className="mysidebar">
-                <li>
-                    <a href="/">Home</a>
-                </li>
-                <li>
-                    <a onClick={() => this.setState({isMenuOpen: !this.state.isMenuOpen})}>About me</a>
-                    {this.state.isMenuOpen ?
-                        <ul className="mysidebar-body">
-                            <li><a href="/">Gihub</a>
-                            </li>
-                            <li><a href="/">Facebook</a>
-                            </li>
-                        </ul>
-                    : ""}
-                </li>
-                <li>
-                    <a onClick={() => this.setState({isMenuOpen: !this.state.isMenuOpen})}>About me</a>
-                    {this.state.isMenuOpen ?
-                        <ul className="mysidebar-body">
-                            <li><a href="/">Gihub</a>
-                            </li>
-                            <li><a href="/">Facebook</a>
-                            </li>
-                        </ul>
-                    : ""}
-                </li>
+                {items.map((item, index) => {
+                    return (
+                        <li key={index}>
+                            {!item.link ? 
+                                <a onClick={() => this.onClickMenu(index)}> {item.label} </a>
+                            : <a href={item.link}> {item.label} </a>}
+
+                            {this.state.menusOpened.includes(index) ? 
+                                <ul className="mysidebar-body">
+                                    {item.submenu.map((subitem,j) => {
+                                        return (
+                                            <li key={j}>
+                                                <a href={subitem.link} >{subitem.label} </a>
+                                            </li>
+                                        )
+                                    })}
+                                </ul>
+                            : ""}
+                        </li>
+                    )
+                })}
             </ul>
         )
     }
